0c24e78420a66c4c8f7174af9a79ed78ee3a5ebf
KilyongJeong <uzoolove@gmail.com>
2021-08-09 11:48:39 +0900

    .

--- hello/helloserver.js
+++ hello/helloserver.js
@@ -1,8 +1,37 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const server = http.createServer(function(req, res){
-  res.writeHead(200);
-  res.write('<h1>Hello HTTP Server~<h1>');
-  res.end();
+  // res.writeHead(200);
+  // res.write('<h1>Hello HTTP Server~<h1>');
+  // res.end();
+
+  var filename = req.url.substring(1);
+  filename = path.join(__dirname, filename);
+
+  // 동기방식의 함수 호출
+  // try{
+  //   let data = fs.readFileSync(filename);
+  //   res.writeHead(200);
+  //   res.write(data);
+  //   res.end();
+  // }catch(err){
+  //   res.writeHead(404);
+  //   res.end('<h1>' + req.url + ' file not found!</h1>');
+  // }
+
+  // 비동기방식의 함수 호출
+  fs.readFile(filename, function(err, data){
+    if(err){
+      res.writeHead(404);
+      res.end('<h1>' + req.url + ' file not found!</h1>');
+    }else{
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+
 });
 
 var port = 8000;
