97c159684099c87c4549d574f1cb3d2ef5051c36
KilyongJeong <uzoolove@gmail.com>
2021-08-10 16:47:41 +0900

    .

--- modules/core/testevents.js
+++ modules/core/testevents.js
@@ -1,6 +1,15 @@
 const EventEmitter = require('events');
+class MyEmitter extends EventEmitter{};
+var emitter = new MyEmitter();
 
-
+emitter.on('newListener', function(eventName, listen){
+  console.log('listener 추가됨.', eventName);
+});
+emitter.addListener('event1', print);
+emitter.on('event1', print);
+emitter.once('event1', print);
+emitter.emit('event1', 'msg-1');
+emitter.emit('event1', 'msg-2');
 
 function print(msg){
   console.log(msg);
