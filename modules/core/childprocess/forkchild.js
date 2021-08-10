626b691a7d08e3a8c932cee3b58de323ceaa5c58
KilyongJeong <uzoolove@gmail.com>
2021-08-10 15:34:57 +0900

    .

--- modules/core/childprocess/forkchild.js
+++ modules/core/childprocess/forkchild.js
@@ -2,3 +2,13 @@ const path = require('path');
 var filename = path.basename(__filename);
 console.log(filename, '실행');
 
+// 2. Chlid <- Parent
+process.on('message', function(data){
+  // 3. Child -> Parent
+  process.send(data);
+});
+
+setTimeout(function(){
+  // process.exit();// 정상 종료
+  a();
+}, 1000);
--- modules/core/childprocess/forkparent.js
+++ modules/core/childprocess/forkparent.js
@@ -1,7 +1,24 @@
 var childProcess = require('child_process');
+const { chdir } = require('process');
 
 // fork(modulePath[, args][, options])
 // node 전용 프로세스를 실행한다.(stdio: 'inherit')
 // spawn()과는 다르게 표준 입출력 장치를 사용할 필요 없이
 // 전용 IPC 채널을 만든다.
 // fork('forkchild.js') == childProcess.spawn('node.exe', ['forkchild.js'], {stdio: 'inherit'});
+var child = childProcess.fork('forkchild.js');
+// 4. Parent <- Child
+child.on('message', function(data){
+  console.log('from child: ' + data);
+});
+// 1. Parent - > Child
+child.send('hello');
+
+// 자식 프로세스가 비정상 종료될 경우
+child.on('exit', function(code){
+  if(code){
+    console.error('에러 발생', code);
+  }else{
+    console.log('정상 종료.');
+  }
+});
