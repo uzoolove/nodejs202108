fd1c84e160dbf5f5ffb82a713d2eb2326bc2f215
KilyongJeong <uzoolove@gmail.com>
2021-08-11 10:38:55 +0900

    .

--- server/net/tcpclient.js
+++ server/net/tcpclient.js
@@ -1,9 +1,25 @@
 const net = require('net');
 var target = {
-  host: 'localhost',
-  port: 2345
+  // host: 'localhost',
+  host: '106.246.114.67',
+  port: 8100
 };
 
 // 1. new.Socket 생성
+// var socket = new net.Socket();
 // 2. 서버 접속
-// 3. 서버와 메세지 송수신
+// socket.connect();
+var socket = net.createConnection(target.port, target.host, function(){
+  console.log(`서버 접속. ${target.host}:${target.port}`);
+  // 3. 서버와 메세지 송수신
+  // socket.write('hello');
+});
+socket.on('data', function(data){
+  console.log('서버로부터 수신한 메세지. ' + data);
+});
+
+// 표준 입력장치로부터 메세지를 읽어서 서버에 전송
+process.stdin.on('data', function(data){
+  socket.write(data);
+});
+
--- server/net/tcpserver.js
+++ server/net/tcpserver.js
@@ -1,4 +1,20 @@
 const net = require('net');
 // 1. net.Server 생성
+// var tcpServer = new net.Server();
+// tcpServer.on('connection', function(){});
+var tcpServer = net.createServer(function(socket){
+  console.log(socket.remoteAddress, '접속함.');
+  // 반드시 error 이벤트를 처리해야 한다.
+  socket.on('error', function(){
+    console.log(socket.remoteAddress, '접속 종료.');
+  });
+  // 3. 클라이언트와 메세지 송수신
+  socket.on('data', function(data){
+    console.log(socket.remoteAddress + ': ' + data);
+    socket.write(data);
+  });
+});
 // 2. 포트 오픈 서버 구동
-// 3. 클라이언트와 메세지 송수신
+tcpServer.listen(8100, function(){
+  console.log('TCP 서버 구동. 8100');
+});
