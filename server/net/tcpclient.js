84cf6d3cd91b684c7ba9df69457230609047f46b
KilyongJeong <uzoolove@gmail.com>
2021-08-11 09:51:17 +0900

    .

--- server/net/tcpclient.js
+++ server/net/tcpclient.js
@@ -3,3 +3,7 @@ var target = {
   host: 'localhost',
   port: 2345
 };
+
+// 1. new.Socket 생성
+// 2. 서버 접속
+// 3. 서버와 메세지 송수신
--- server/net/tcpserver.js
+++ server/net/tcpserver.js
@@ -1,1 +1,4 @@
 const net = require('net');
+// 1. net.Server 생성
+// 2. 포트 오픈 서버 구동
+// 3. 클라이언트와 메세지 송수신
