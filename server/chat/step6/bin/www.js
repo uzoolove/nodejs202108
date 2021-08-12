22408ab17963b973e46d627af358a8f97affa7b5
KilyongJeong <uzoolove@gmail.com>
2021-08-12 14:15:24 +0900

    .

--- server/chat/step6/bin/www.js
+++ server/chat/step6/bin/www.js
@@ -3,7 +3,7 @@ const app = require('../app');
 
 var tcpServer = http.createServer(app);
 // 2. 포트 오픈 서버 구동
-tcpServer.listen(80, function(){
+tcpServer.listen(8100, function(){
   console.log('HTTP 서버 구동. 80');
 });
 
