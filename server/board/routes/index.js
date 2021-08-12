954da3cbeecd8a25c2d323d764ba6da2588bec65
KilyongJeong <uzoolove@gmail.com>
2021-08-12 15:12:43 +0900

    .

--- server/board/routes/index.js
+++ server/board/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 메인페이지
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '게시판' });
+  res.redirect('/board');
+});
+// 목록 조회
+router.get('/board', function(req, res, next) {
+  res.render('board/list', { title: '게시물 목록' });
+});
+// 등록 화면 요청
+router.get('/board/new', function(req, res, next) {
+  res.render('board/write', { title: '글쓰기' });
+});
+// 등록 요청
+router.post('/board/new', function(req, res, next) {
+  res.render('board/result', { title: '등록 결과' });
+});
+// 상세 조회
+router.get('/board/:no', function(req, res, next) {
+  res.render('board/view', { title: '내용 조회' });
+});
+// 삭제
+router.post('/board/:no', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
