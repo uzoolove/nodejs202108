608ca142a9bdeaaaee67c506edc6163859bd24f5
KilyongJeong <uzoolove@gmail.com>
2021-08-13 11:42:48 +0900

    .

--- server/board/test/test-assert.js
+++ server/board/test/test-assert.js
@@ -5,6 +5,7 @@ var success = 0;
 var fail = 0;
 console.log('테스트 시작.');
 
+/*
 var result = upper.sync('hello', 2);
 if(result == 'L'){
   console.log('1. 통과');
@@ -14,6 +15,63 @@ if(result == 'L'){
   fail++;
 }
 
-console.log('테스트 종료.');
-console.log('success', success);
-console.log('fail', fail);
+var result = upper.sync('hello', 5);
+if(result == null){
+  console.log('2. 통과');
+  success++;
+}else{
+  console.log('2. 실패');
+  fail++;
+}
+*/
+
+/* 독립적인 테스트를 위해서 작성해야 하는 코드가 너무 많다.
+try{
+  var result = upper.sync('hello', 2);
+  assert.equal(result, 'L');
+  console.log('1. 통과');
+  success++;
+}catch(err){
+  console.log('1. 실패', err.message);
+  fail++;
+}
+try{
+  var result = upper.sync('hello', 5);
+  assert(result == null);
+  console.log('2. 통과');
+  success++;
+}catch(err){
+  console.log('2. 실패', err.message);
+  fail++;
+}
+*/
+
+// 비동기 함수 테스트(순차적인 테스트가 필요한 경우 코드가 복잡해진다.)
+upper.async('hello', 5, function(result){
+  try{
+    assert(result == null);
+    console.log('1. 통과');
+    success++;
+  }catch(err){
+    console.log('1. 실패', err.message);
+    fail++;
+  }
+  upper.async('hello', 2, function(result){
+    try{
+      assert.equal(result, 'L');
+      console.log('2. 통과');
+      success++;
+    }catch(err){
+      console.log('2. 실패', err.message);
+      fail++;
+    }
+    console.log('테스트 종료.');
+    console.log('success', success);
+    console.log('fail', fail);
+  });
+});
+
+
+// console.log('테스트 종료.');
+// console.log('success', success);
+// console.log('fail', fail);
