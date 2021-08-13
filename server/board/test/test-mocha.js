ca3e9aa20d62f152550df508e042be3b1d3e7d4b
KilyongJeong <uzoolove@gmail.com>
2021-08-13 11:53:54 +0900

    .

--- server/board/test/test-mocha.js
+++ server/board/test/test-mocha.js
@@ -4,4 +4,18 @@ var model = require('../models/board');
 
 var article = {writer: '김철수', title: 'mocha 테스트', content: '김철수 만세'};
 
-
+// test suite
+describe('# upper 함수 테스트', function(){
+  describe('# 동기 테스트', function(){
+    // unit test
+    it('sync(hello, 2)', function(){
+      assert(upper.sync('hello', 2) === 'L');
+    });
+    it('sync(hello, 5)', function(){
+      assert.strictEqual(upper.sync('hello', 5), null);
+    });
+    it('sync(hello, 0)', function(){
+      assert.strictEqual(upper.sync('hello', 0), 'H');
+    });
+  });
+});
